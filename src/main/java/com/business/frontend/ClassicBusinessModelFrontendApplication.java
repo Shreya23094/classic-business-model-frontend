8182bc8362f69824870e099e6250872cf9a9a5fe
Vaduni Niranjan <vaduniniranjan@gmail.com>
2026-04-02 14:22:37 +0530

    initial thymeleaf frontend setup

---
+++ src/main/java/com/business/frontend/ClassicBusinessModelFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.business.frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassicBusinessModelFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassicBusinessModelFrontendApplication.class, args);
+	}
+
+}
